c841812c17e447f7cfd9c85c423da8f4fed4191c
Lukas Krickl <lukas@krickl.dev>
2023-05-05 10:29:22 +0200

    Added hash libraries

--- src/core/runner/jwt.rs
+++ src/core/runner/jwt.rs
@@ -1,7 +1,27 @@
-#[derive(Clone)]
+use crate::core::transform::Word;
+use hmac::{Hmac, Mac};
+use sha2::Sha256;
+
+#[derive(Clone, Default)]
 pub enum Signature {
-    Hmac { secret: Vec<u8> },
+    HmacSha256 {
+        secret: Word,
+    },
+    #[default]
     None,
+}
+
+impl Signature {
+    pub fn sign(&self, data: &Word) -> Word {
+        match self {
+            Signature::HmacSha256 { secret } => {
+                type HmacSha256 = Hmac<Sha256>;
+                let mac = HmacSha256::new_from_slice(&secret).expect("");
+                mac.finalize().into_bytes().iter().map(|x| *x).collect()
+            }
+            Signature::None => data.to_owned(),
+        }
+    }
 }
 
 #[derive(Clone)]
